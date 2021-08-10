cddc9905e40175a2d98a5a115ac8cd0aaaba11d8
Uray Irvan <urayirvan@gmail.com>
2021-08-10 13:48:32 +0700

    export playlists implemented

---
+++ src/api/exports/handler.js
@@ -0,0 +1,56 @@
+const ClientError = require('../../exceptions/ClientError');
+
+class ExportsHandler {
+  constructor(exportsService, playlistsService, validator) {
+    this._exportsService = exportsService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+
+    this.postExportPlaylistsHandler = this.postExportPlaylistsHandler.bind(this);
+  }
+
+  async postExportPlaylistsHandler(request, h) {
+    try {
+      this._validator.validateExportPlaylistsPayload(request.payload);
+      const { id: credentialId } = request.auth.credentials;
+      const { playlistId } = request.params;
+
+      await this._playlistsService.verifyPlaylistAccess(playlistId, credentialId);
+
+      const message = {
+        userId: credentialId,
+        playlistId,
+        targetEmail: request.payload.targetEmail,
+      };
+
+      await this._exportsService.sendMessage('export:playlists', JSON.stringify(message));
+
+      const response = h.response({
+        status: 'success',
+        message: 'Permintaan Anda sedang kami proses',
+      });
+      response.code(201);
+      return response;
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: 'fail',
+          message: error.message,
+        });
+        response.code(error.statusCode);
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami.',
+      });
+      response.code(500);
+      console.error(error);
+      return response;
+    }
+  }
+}
+
+module.exports = ExportsHandler;
--- src/server.js
+++ src/server.js
@@ -35,6 +35,11 @@ const collaborations = require('./api/collaborations');
 const CollaborationsService = require('./services/postgres/CollaborationsService');
 const CollaborationsValidator = require('./validator/collaborations');
 
+// exports
+const _exports = require('./api/exports');
+const ProducerService = require('./services/rabbitmq/ProducerService');
+const ExportsValidator = require('./validator/exports');
+
 const init = async () => {
   const songsService = new SongsService();
   const usersService = new UsersService();
@@ -119,6 +124,14 @@ const init = async () => {
       collaborationsService,
       playlistsService,
       validator: CollaborationsValidator,
+    },
+  },
+  {
+    plugin: _exports,
+    options: {
+      exportsService: ProducerService,
+      playlistsService,
+      validator: ExportsValidator,
     },
   },
   ]);
